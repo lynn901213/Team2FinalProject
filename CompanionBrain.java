3c6d5432700d0c8d69facf02af8b53236d2f3ce4
Kyle Sun <kysun@asu.edu>
2017-07-03 01:30:36 -0700

    Delete generateed base classes, added some base files and images.

--- CompanionBrain.java
+++ CompanionBrain.java
@@ -1,4 +1,78 @@
 package CSE360;
-public class CompanionBrain {
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/*
+CSE360 Summer 2017
+Kyle Sun
+Jingyi Li
+Lin Sun
+*/
+
+public class CompanionBrain {
+   JSONObject jsonObj;
+   private static java.lang.String key;
+   
+   /**
+   * Create a GetWeather with the given API key for DarkSky.
+   */
+   public CompanionBrain(java.lang.String wKey) {
+      key = wKey;
+      try {
+         setWeather(0, 0);
+      } catch (IOException | JSONException e) {
+         java.lang.System.out.println(e);
+         java.lang.System.exit(1);
+      }
+   }
+   /**
+   * Sets the weather info to the location based on given double parameters for
+   * Latitude and Longitude.
+   */
+   public void setWeather(double lat, double lon) throws IOException, JSONException{
+      java.lang.String weaUrl = "https://api.darksky.net/forecast/" + key + "/" + lat + "," + lon;
+      URL url = new URL(weaUrl);
+      URLConnection wc = url.openConnection();
+      BufferedReader in = new BufferedReader(
+      new InputStreamReader(wc.getInputStream()));
+      java.lang.String inputLine;
+      java.lang.String json = "";
+      // Read response and add to string
+      while ((inputLine = in.readLine()) != null) {
+         json = json + inputLine;
+      }
+      in.close();
+      jsonObj = new JSONObject(json);
+   }
+   
+   
+   /**
+   * Returns the current condition as a String based on DarkSky's formatting.
+   */
+   public java.lang.String getCondition() throws JSONException{
+      return jsonObj.getJSONObject("currently").getString("icon");
+   }
+   
+   /**
+   * Returns information displayed as a Double based on DarkSky's formatting
+   * and the given string of information requested.
+   */
+   public double getDoubleInfo(java.lang.String info) throws JSONException{
+      return jsonObj.getJSONObject("currently").getDouble(info);
+   }
+   
+   /**
+   * Returns information displayed as an Integer based on DarkSky's formatting
+   * and the given string of information requested.
+   */
+   public int getIntInfo(java.lang.String info) throws JSONException{
+      return jsonObj.getJSONObject("currently").getInt(info);
+   }
 }
+
--- CompanionPanel.java
+++ CompanionPanel.java
@@ -1,16 +1,83 @@
 package CSE360;
-public class CompanionPanel implements Runnable {
+/*
+CSE360 Summer 2017
+Kyle Sun
+Jingyi Li
+Lin Sun
+*/
+import javax.swing.*;
+import java.io.*;
+import java.util.Random;
 
-	public void run() {
+public class CompanionPanel extends JLabel implements Runnable {
 
-	}
+/** The ghost moves and bounces around the screen.*/
+   public int x;
+   public int y;
+   private int hspeed;
+   private int vspeed;
+   private int frameHeight;
+   private int frameWidth;
+   private JLabel companion;
+   Random r;   
+    /** Create a Ghost with the provided file paths for the ghost image moving
+    right and left.*/
+    public CompanionPanel(java.lang.String img, int width, int height) throws IOException {
+       companion = new JLabel(new ImageIcon(img));
+       add(companion);
+       frameWidth = width;
+       frameHeight = height;
+       r = new Random();
+       x = r.nextInt(frameWidth - 96) + 48;
+       y = r.nextInt(frameHeight - 96) + 48;
+       hspeed = r.nextInt(3) + 1;
+       vspeed = r.nextInt(3) + 1;
+    }
+    /** Set the bounds of the frame for the ghost to bounce off of. If bounds
+    * are set past visibility, ghost is moved within the frame.
+    */
+    public void setFrame(int n, int j) {
+       frameWidth = n;
+       frameHeight = j;
+       if (x > frameWidth) {
+          x = frameWidth - 33;
+       }
+       if (y > frameHeight) {
+          y = frameHeight - 33;
+       }
+    }
 
-	public void drawCompanion() {
+    /** Move the ghost around the screen and bounces it off the frame boundaries. */
+    @Override
+    public void run() {
+       while(true) {
+          try {
+             Thread.sleep(25);
+          } catch (InterruptedException i) {
+             java.lang.System.exit(1);
+          }
+          drawCompanion();
+          x += hspeed;
+          y += vspeed;
+          if (x < 1 || x + 33 > frameWidth) {
+             hspeed *= -1;
+          }
+          if (y < 1 || y + 33 > frameHeight) {
+             vspeed *= -1;
+          }
+       }
+    }
+    
+    public void setImage(String img) {
+        companion.setIcon(new ImageIcon(img));
+    }
 
-	}
+    public void drawCompanion() {
+        companion.setBounds(x, y, 32, 32);
+    }
 
-	public void drawMessage() {
+    public void drawMessage() {
 
-	}
+    }
 
 }
